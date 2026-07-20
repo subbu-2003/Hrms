678bf02fdacf733e868962b67a04f749fa2a9327
Kiruthika-308 <kiruthika.rengarajan@gmail.com>
2026-07-20 21:38:50 +0530

    change the app ID

--- src/Components/FacebookComponent/Facebooklogin.tsx
+++ src/Components/FacebookComponent/Facebooklogin.tsx
@@ -253,7 +253,7 @@ const handleFacebookLogin = () => {
     {
       // pages_show_list,pages_read_engagement,business_management,leads_retrieval,pages_manage_ads
       scope:
-        "public_profile,pages_show_list,pages_read_engagement,business_management,leads_retrieval,pages_manage_ads",
+        "public_profile",
       auth_type: "rerequest",
       return_scopes: true,
     }
